6035a82f3e4716f425d0440f147d3577632d4ba6
Simon Weller <s.meierbeck@gmail.com>
2017-08-20 15:00:45 +0200

    setting up templating

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const engine = require('express-ejs-layouts');
+const morgan = require('morgan');
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(engine);
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'The index page!'});
+});
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/votingapp', {
+  useMongoClient: true
+}).then(
+  () => {
+    app.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    });
+  },
+  err => {
+    throw err;
+  }
+);
